cd70d8f514b08babf3eb51aa46d71c6222ba1cb6
Siim Laineste <siim.laineste@outlook.com>
2021-09-16 10:51:52 +0300

    get last db entry

--- dbGet.py
+++ dbGet.py
@@ -6,7 +6,10 @@ def getData():
     db = client['coordinates']
     collection = db['CoordPlot']
     #query for latt and longtitude
-    
-    for x in collection.find({} , {"major.inlatt" : 1 , "major.inlongt" : 1}):
-        print(x)
-        
+    #global query_coords
+    #query_coords = {"major.inlatt" : 1 , "major.inlongt" : 1}
+    #for x in collection.find({} , {"major.inlatt" : 1 , "major.inlongt" : 1}):
+    #    print(x)
+#find_one should retrieve the latest one added if there are multiple matches.
+    last_coord = collection.find_one({}, {"major.inlatt" : 1 , "major.inlongt" : 1})
+    print(last_coord)
